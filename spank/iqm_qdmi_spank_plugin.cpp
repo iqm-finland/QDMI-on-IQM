731498bc39f162a638ef7e13b327de38e60854db
Marcel Walter <marcel.walter@tum.de>
2026-06-12 13:56:28 +0200

    📄 Adjust license tools config to use the GPLv3 tag directly

--- spank/iqm_qdmi_spank_plugin.cpp
+++ spank/iqm_qdmi_spank_plugin.cpp
@@ -1,21 +1,20 @@
 /*
- * Copyright (c) 2025 - 2026 IQM Finland Oy
+ * Copyright (c) 2026 IQM Finland Oy
  * All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You may obtain a copy of the License at
- *
- * https://github.com/iqm-finland/QDMI-on-IQM/blob/main/spank/LICENSE.md
- *
  * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 /** @file
